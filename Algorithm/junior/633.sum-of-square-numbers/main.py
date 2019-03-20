cbf060be5c848b8aad587106e018e618e06f7013
rodney.p.yang <rodney.p.yang@cnsdc01.pwc.com>
2019-03-20 15:28:48 +0800

    success_633_py

--- Algorithm/junior/633.sum-of-square-numbers/main.py
+++ Algorithm/junior/633.sum-of-square-numbers/main.py
@@ -1,15 +1,14 @@
 # endcoding: utf-8
 import math
 
-
+# 暴力法
 def judgeSquareSum(c):
-    if c<2:
-        return False 
-
-
     base = int(math.sqrt(c))
     if c == 1 + base* base:
         return True
-    for i in xrange(int(base)):
-        
-        
+    for i in range(base, 0, -1):
+        m = math.sqrt(c - i*i)
+        if m == int(m):
+            return true
+    return False
+print judgeSquareSum(999999999)
